f4e03e89aa8d1369aad3c886922457da879a8675
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-04-04 14:47:49 +0200

    AccessTokenReader

--- QuizuApi/Program.cs
+++ QuizuApi/Program.cs
@@ -2,10 +2,15 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using QuizuApi.Data;
 using QuizuApi.Models;
+using QuizuApi.Repository;
+using QuizuApi.Repository.IRepository;
+using QuizuApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IAccessTokenCreatorService, AccessTokenCreatorService>();
+builder.Services.AddScoped<IAccessTokenReaderService, AccessTokenReaderService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ QuizuApi/Services/AccessTokenReaderService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Primitives;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace QuizuApi.Services
+{
+    public class AccessTokenReaderService : IAccessTokenReaderService
+    {
+        public string ReadUserId(string jwtToken)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.ReadJwtToken(jwtToken);
+            var userIdClaim = token.Claims.FirstOrDefault(c => c.Type == "nameid");
+
+            if (userIdClaim is null)
+            {
+                throw new Exception("Invalid access token.");
+            }
+            return userIdClaim.Value;
+        }
+
+        public string? RetrieveUserIdFromRequest(HttpRequest request)
+        {
+            if (request.Headers.ContainsKey("Authorization"))
+            {
+                if (request.Headers.TryGetValue("Authorization", out StringValues values))
+                {
+                    var jwt = values.ToString();
+
+                    if (jwt.Contains("Bearer"))
+                    {
+                        jwt = jwt.Replace("Bearer", "").Trim();
+                    }
+
+                    try
+                    {
+                        return ReadUserId(jwt);
+                    }
+                    catch (Exception)
+                    {
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
