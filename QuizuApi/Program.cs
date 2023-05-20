5b308e3cd276f041c450d7a22cee01f048e250ed
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-05-20 19:16:40 +0200

    swagger

--- QuizuApi/Program.cs
+++ QuizuApi/Program.cs
@@ -103,12 +103,12 @@ builder.Services.AddCors(opt =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "StoryHubAPI");
+    options.RoutePrefix = String.Empty;
+});
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
